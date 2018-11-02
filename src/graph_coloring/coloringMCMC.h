ccd9b0d1df8ac0ec83bf8e1c1239f7bfd9440485
Lemen47Nic <aspes.nicholas@hotmail.it>
2018-11-02 17:41:23 +0100

    new counters

--- src/graph_coloring/coloringMCMC.h
+++ src/graph_coloring/coloringMCMC.h
@@ -33,27 +33,28 @@ protected:
 	//dati del grafo
 	const GraphStruct<nodeW, edgeW>	* const	graphStruct_d;
 
-	int				counter;
-	int				newCounter;
+	int				conflictCounter;
+	int				conflictCounterStar;
 
 	//int *ret;
-	uint32_t	*	counter_h; // lo spazio occupato può essere grande quanto blocksPerGrid_half_edges se si usa solo la somma parallela
-	uint32_t	*	counter_d;
+
+	uint32_t	*	conflictCounter_h; // lo spazio occupato può essere grande quanto blocksPerGrid_half_edges se si usa solo la somma parallela, vale anche per gli altri punti in cui si usa la somma parallela
+	uint32_t	*	conflictCounter_d;
 
 
 	float			result, random;
 
-	uint32_t	*	temp;
 	uint32_t	*	coloring_d;			// each element denotes a color
-	uint32_t	*	newColoring_d;		// each element denotes a new color
+	uint32_t	*	starColoring_d;		// each element denotes a new color
+	uint32_t	*	switchPointer;
 
-	float			probColoring;
-	float		*	probColoring_h;
-	float		*	probColoring_d;		// each element denotes a probability for a color
+	float			p;
+	float		*	q_h;
+	float		*	q_d;				// each element denotes a probability for a color
 
-	float			probNewColoring;
-	float		*	probNewColoring_h;
-	float		*	probNewColoring_d;	// each element denotes a probability for a new color
+	float			pStar;
+	float		*	qStar_h;
+	float		*	qStar_d;	// each element denotes a probability for a new color
 
 	bool		*	colorsChecker_d;
 	uint32_t	*	orderedColors_d;
@@ -73,7 +74,7 @@ protected:
 	dim3			blocksPerGrid_half_edges;
 	curandState *	randStates;
 
-	void			calcConflicts(int &counter, uint32_t * coloring_d);
+	void			calcConflicts(int &conflictCounter, uint32_t * coloring_d);
 	void			getStats();
 	void			calcProbs();
 };
@@ -82,9 +83,9 @@ protected:
 namespace ColoringMCMC_k {
 	__global__ void initColoring(uint32_t nnodes, uint32_t * coloring_d, float divider, curandState * states);
 	__global__ void logarithmer(uint32_t nnodes, float * values);
-	__global__ void conflictChecker(uint32_t nedges, uint32_t * counter_d, uint32_t * coloring_d, node_sz * edges);
-	__global__ void sumReduction(uint32_t nedges, float * counter_d);
+	__global__ void conflictChecker(uint32_t nedges, uint32_t * conflictCounter_d, uint32_t * coloring_d, node_sz * edges);
+	__global__ void sumReduction(uint32_t nedges, float * conflictCounter_d);
 	__device__ void warpReduction(volatile float *sdata, uint32_t tid, uint32_t blockSize);
-	__global__ void selectNewColoring(uint32_t nedges, uint32_t * newColoring_d, float * probNewColoring, col_sz nCol, uint32_t * coloring_d, node_sz * cumulDegs, node * neighs, bool * colorsChecker_d, uint32_t * orderedColors_d, curandState * states, float epsilon, uint32_t * statsFreeColors_d);
-	__global__ void lookOldColoring(uint32_t nedges, float * probColoring_d, col_sz nCol, uint32_t * newColoring_d, uint32_t * coloring_d, node_sz * cumulDegs, node * neighs, bool * colorsChecker_d, float epsilon);
+	__global__ void selectStarColoring(uint32_t nnodes, uint32_t * starColoring_d, float * qStar_d, col_sz nCol, uint32_t * coloring_d, node_sz * cumulDegs, node * neighs, bool * colorsChecker_d, uint32_t * orderedColors_d, curandState * states, float epsilon, uint32_t * statsFreeColors_d);
+	__global__ void lookOldColoring(uint32_t nnodes, float * q_d, col_sz nCol, uint32_t * starColoring_d, uint32_t * coloring_d, node_sz * cumulDegs, node * neighs, bool * colorsChecker_d, float epsilon);
 }
