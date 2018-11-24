cdf402692b4b4b40d65985de98838e8cab7d2891
A. Petrini <alessandro.petrini@unimi.it>
2018-11-24 18:01:59 +0100

    minor funcitions moved to MCMCutils

--- src/graph_coloring/coloringMCMC_CPU.cpp
+++ src/graph_coloring/coloringMCMC_CPU.cpp
@@ -469,46 +469,6 @@ void ColoringMCMC_CPU<nodeW, edgeW>::fill_qstar(const size_t currentNode, const 
 	}
 }
 
-template<typename nodeW, typename edgeW>
-void ColoringMCMC_CPU<nodeW, edgeW>::show_histogram() {
-	std::vector<size_t> histBins(nCol, 0);
-	std::vector<size_t> histBinsOcc(nCol, 0);
-	// Initial check: each value should be 0 <= val < nCol
-	size_t idx = 0;
-	std::for_each( std::begin(C), std::end(C), [&](uint32_t val) { if ((val < 0) | (val >= nCol)) std::cout << "Error in color " << idx << ". Exiting..." << std::endl; idx++;} );
-
-	// Filling histogram bins
-	idx = 0;
-	std::for_each( std::begin(C), std::end(C), [&](uint32_t val) { histBinsOcc[val] += (Cviols[idx++] == 1);} );
-	idx = 0;
-	std::for_each( std::begin(C), std::end(C), [&](uint32_t val) { histBins[val] += (Cviols[idx++] == 0);} );
-
-	// finding the correct scaler, so that the maximum bin get printed 80 times (ignoring rounding errors...)
-	std::vector<size_t> tempVec(nCol);
-	idx = 0;
-	std::for_each( std::begin(tempVec), std::end(tempVec), [&](size_t &val) { val = histBinsOcc[idx] + histBins[idx]; idx++;} );
-	auto max_val = std::max_element( std::begin(tempVec), std::end(tempVec) );
-	size_t scaler = *max_val / 80;
-
-	// Printing the histogram.
-	for (size_t i = 0; i < histBins.size(); i++) {
-		std::cout << i << ": ";
-
-		size_t howMany = histBins[i] / scaler;
-		std::cout << TXT_BIGRN;
-		for (size_t j = 0; j < howMany; j++)
-			std::cout << "*";
-
-		howMany = histBinsOcc[i] / scaler;
-		std::cout << TXT_BIRED;
-		for (size_t j = 0; j < howMany; j++)
-			std::cout << "*";
-
-		std::cout << TXT_NORML << std::endl;
-	}
-	std::cout << "Each '*' = " << scaler << " nodes" << std::endl;
-}
-
 
 /////////////////////
 template class Graph<float, float>;
